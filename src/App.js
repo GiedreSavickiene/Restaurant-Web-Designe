b2eb5dd728697f8d68720c4789048bd363234957
GiedreSavickiene <78999134+GiedreSavickiene@users.noreply.github.com>
2022-07-31 17:09:24 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react'
-import ScrollToTop from './components/ScrollToTop'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Services from './components/Services'
-import Portfolio from './components/Porfolio'
-import Testimonials from './components/Testimonials'
-import Products from './components/Products'
-import Newsletter from './components/Newsletter'
-import Footer from './components/Footer'
+import './App.css';
 
 const App = () => {
   return (
     <>
-      <ScrollToTop />
-      <Navbar />
-      <Hero />
-      <Services />
-      <Portfolio />
-      <Testimonials />
-      <Products />
-      <Newsletter />
-      <Footer />
+      labas
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
-import styled from 'styled-components'
-import logo from '../assets/images-logo.png'
-
-const Nav = styled.nav``;
 
 const Navbar = () => {
   return (
-    <Nav>
-      <div className="brand">
-        <img src={logo} alt="logo" />
-      </div>
-    </Nav>
+    <div>
+      
+    </div>
   )
 }
 
