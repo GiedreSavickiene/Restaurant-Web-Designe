84b21ecc77e39f6763edf836992dd2d587a94acd
GiedreSavickiene <78999134+GiedreSavickiene@users.noreply.github.com>
2022-08-01 16:41:37 +0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <>
-      labas
-    </>
+    <div>
+      <Header />
+    </div>
   )
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='baner'>
+            <div className="baner_content">
+                <div className='container'>
+                    <div className="baner_text">
+                        <h3>Food Delivery</h3>
+                        <h1>NATURAL MARKET</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, molestias magni eum neque harum odio enim porro? Debitis suscipit omnis, eos incidunt est repudiandae!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
